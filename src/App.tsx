e185bb1eb409bb5da94d3ca5ad62d862f4a58188
artem_vasiliev <vincere802@gmail.com>
2021-03-12 14:34:56 +0200

    HW4 done

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import HW1 from "./h1/HW1";
 import HW2 from "./h2/HW2";
 import HW3 from "./h3/HW3";
+import HW4 from "./h4/HW4";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <HW1/>
       <HW2/>
       <HW3/>
+      <HW4/>
     </div>
   );
 }
